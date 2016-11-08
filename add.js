1d4947a22a6109e9e2ca129ce1bdd1ae6e0b9bff
Martin <martinconradpelayo@gmail.com>
2016-11-08 13:14:32 -0800

    Tuesday_11_8_2016

---
+++ add.js
@@ -0,0 +1,9 @@
+'use strict';
+var firstYesorNOquestionAnswer =prompt ('Do yah like ice cream?')
+if (firstYesorNOquestionAnswer === 'yes'){
+  alert ('Sweet, me too!');
+}else{
+  alert('what is wrong with you literally everybody ever odes.');
+}
+}
+console.log ('hello from app.js');
