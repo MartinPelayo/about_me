dd7bd23a6e633cfeed841c2839c6147f472797d1
Martin <martinconradpelayo@gmail.com>
2016-11-13 21:00:51 -0800

    lab11-9

--- add.js
+++ add.js
@@ -1,64 +1,74 @@
 'use strict';
-var yesQuestion1 = prompt ('Would you like a burrito?').toUpperCase();
+alert ('Get to know me better as a future developer with my guessing game, give it a shot.');
+
+var yesQuestion1 = prompt ('Do I want to be a developer?').toUpperCase();
 if (yesQuestion1 === 'yes'){
-  alert ('Splendid, would you like cheese on your burrito?');
+  alert ('Yes I would like to be a developer.');
 }else{
-  alert('Thank you come agian.');
+  alert('Actually I would like to be a developer.');
 }
-var yesQuestion2 = prompt ('Would you like hot sauce?').toUpperCase();
-if (yesQuestion2 === 'yes'){
-  alert ('All right my friend.');
+
+var yesQuestion2 = prompt ('Where am I going to pursue this goal?').toUpperCase();
+if (yesQuestion2 === 'CodeFellows'){
+  alert ('Thats Correct.');
 }else{
-  alert ('Sorry, no hot suace no service.');
+  alert ('The answer is CodeFellows.');
 }
-var yesQuestion3 = prompt ('Can I get you a cup of tea with that?').toUpperCase();
-if (yesQuestion3 === 'yes'){
-  alert ('Tea it is.');
+
+var yesQuestion3 = prompt ('What class am I currently in?').toUpperCase();
+if (yesQuestion3 === '201'){
+  alert ('That is correct.');
 }else{
-  alert ('Ok, no tea then.');
+  alert ('The answer is 201.');
 }
-var yesQuestion4 = prompt ('How about desert?').toUpperCase();
-if (yesQuestion4 === 'yes'){
-  alert ('Alright desert, great.');
+
+var yesQuestion4 = prompt ('What languages am I learning?').toUpperCase();
+if (yesQuestion4 === 'HTML, CSS and JavaScript'){
+  alert ('Correct.');
 }else{
-  alert ('Ok, thats fine.');
+  alert ('The answer is HTML, CSS and JavaScript.');
 }
-var yesQuestion5 = prompt ('Would you like cheese cake then?').toUpperCase();
-if (yesQuestion5 === 'yes'){
-  alert ('You got it boss.');
+
+var yesQuestion5 = prompt ('What type of development would I like to get into?').toUpperCase();
+if (yesQuestion5 === 'Front end development'){
+  alert ('That is correct.');
 }else{
-  alert ('Alright, how about the carrot cake then.');
-}
-var keepLooping = true;
-while(keepLooping !== false){
-
+  alert ('The answer is front end development.');
 }
 
-for (var i = 0; i < 4; i++) {
+// var keepLooping = true;
+// while(keepLooping !== false){
+// }
+//
+// for (var i = 0; i < 4; i++) {
+//
+//   var answer = prompt ('Guess what number out of ten?');
+//
+//   if (answer === '5'){
+//     alert ('Correct.');
+//     keepLooping = false;
+//
+//   }else if (answer > '5'){
+//     alert ('To low, guess again.');
+//   }else if (answer < '5'){
+//     alert ('To high, keep geussing.');
+//   }
+// }
+//
+// console.log;
+//
+// var myArray = ['California,Oregon,Arizona,Texas'];
+// for (var i = 0; i< 6; i++){
+//   var answer = prompt ('Can you guess a states I lived in besides Washington?')
+//   for (var j = 0; j<myArray.legnth; j++){
+//     if (answer === myArray[j])
+//       alert ('Correct');
+//
+//     }else{
+//       alert ('Geuss agian')
+//     }
+//   }
+// }
+// console.log;
 
-var answer = prompt ('Guess what number out of ten?');
-
-if (answer === '5'){
-alert ('Correct.');
-keepLooping = false;
-
-}else if (answer > '5'){
- alert ('To low, guess again.')
-}else if (answer < '5'){
-alert ('To high, keep geussing.')
-}
-}
-
-var myArray = ['California,Oregon,Arizona,Texas'];
-for (var i = 0; i< 6; i++){
-  var answer = prompt ('Can you guess a states I lived in besides Washington?')
-  for (var j = 0; j<myArray.legnth; j++){
-    if (answer === myArray[j])
-      alert ('Correct');
-
-    }
-    else {
-      alert ('Geuss agian')
-    }
-  }
-}
+// I commented out the above code beause it kept crashing in the browser and/or wouldnt run.
